202f230512b1740b890f07260912439713cbd020
willthequeencome <161780156+willthequeencome@users.noreply.github.com>
2024-11-03 06:20:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const Fastify = require("fastify");
-const server = Fastify();
+//hiuu
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-server.register(require("@fastify/http-proxy"), {
-	upstream: "https://gfgfgf.pages.dev/",
-	prefix: "/",
-	http2: false,
+const app = express();
+
+
+const target = 'http://streamgeek.xyz:25461';
+
+// Create the proxy middleware
+const mathProxy = createProxyMiddleware({
+  target,
+  changeOrigin: true,
+  logLevel: 'debug',
+  onProxyRes: function(proxyRes, req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  }
 });
 
-server.listen({ host: "0.0.0.0", port: 8000 }, () => {
-	console.log("listening on port 8000");
+
+// Use the proxy middleware for all requests
+app.use('/', mathProxy);
+
+// Start the Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
